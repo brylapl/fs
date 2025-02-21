952dce7cd5f865f8eb11e6152b0a55eac61bcce4
brylapl <92885606+brylapl@users.noreply.github.com>
2025-02-21 12:40:30 +0100

    Update app.py

--- app.py
+++ app.py
@@ -43,64 +43,34 @@ try:
         dbname=DBNAME
     )
     st.write("Connection successful!")
-    
     # Create a cursor to execute SQL queries
     c = conn.cursor()
 
-    
-
     driver = web_driver()
-
     driver.get('https://www.flashscore.pl/')
+    try:
+        cookies = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, '//button[@id="onetrust-accept-btn-handler"]')))
+        cookies.click()
+    except:
+        pass
+
+    
     st.write(driver.title)
-    all_match = driver.find_elements(By.XPATH,'//*[@title="Zobacz szczegóły meczu!"]')
+    all_match = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, '//*[@title="Zobacz szczegóły meczu!"]')))
     ilosc = len(all_match)
     st.write(ilosc)
     data = '2025-02-21'
     tabela = []
 
-    for mecz in all_match[0:6]:
-            liga = mecz.find_element(By.XPATH,'./preceding::div[contains(@class,"wclLeagueHeader")][1]/div[2]/div[1]/div[2]/span[1]').text
-            rozgrywki = mecz.find_element(By.XPATH,'./preceding::div[contains(@class,"wclLeagueHeader")][1]/div[2]/div[1]/div[2]/a').text
-            id = mecz.find_element(By.XPATH,'./..')
-            id = id.get_attribute('id')
-            id = id[4:]
-            ilosc -= 1
-            url = f'https://www.flashscore.pl/mecz/{id}/#/zestawienie-kursow/kursy-1x2/koniec-meczu'
-            time = mecz.find_element(By.XPATH,'./following-sibling::div[1]').text
-            home = mecz.find_element(By.XPATH,'./following-sibling::div[2]').text
-            away = mecz.find_element(By.XPATH,'./following-sibling::div[3]').text
-            stats = {
-                'data': data,
-                'time':time,
-                'home': home,
-                'away': away,
-                'Url':url,
-                }
-            tabela.append(stats)
-    df = pd.DataFrame(tabela)
-
-    records_home = df.to_records(index=False)
-    list_of_tuples_home = list(records_home)
-
-    c.executemany('INSERT INTO match (data, time, home, away, url) VALUES (?,?,?,?,?)',list_of_tuples_home)
-    conn.commit()
-
-    try:
-        # Example query
-        c.execute("SELECT * FROM match")
-        result = c.fetchone()
-        st.write('Data:', result[3])
-        st.write('Home:', result[4])
-    except:
-        st.write('Brak danych')
-
-    # Close the cursor and connection
-    c.close()
-    conn.close()
-    st.write("Connection closed.")
+    
 
 except Exception as e:
     st.write(f"Failed to connect: {e}")
+
+finally:
+    c.close()
+    conn.close()
+    st.write("Connection closed.")
+    
 
 st.title('Test')
