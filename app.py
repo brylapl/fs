40978d0755ecd2327c03388a8b4564704a18f361
brylapl <92885606+brylapl@users.noreply.github.com>
2025-02-21 11:54:17 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,44 @@
 import streamlit as st
 
+import psycopg2
+from dotenv import load_dotenv
+import os
+
+# Load environment variables from .env
+load_dotenv()
+
+# Fetch variables
+USER = os.getenv("user")
+PASSWORD = os.getenv("password")
+HOST = os.getenv("host")
+PORT = os.getenv("port")
+DBNAME = os.getenv("dbname")
+
+# Connect to the database
+try:
+    connection = psycopg2.connect(
+        user=USER,
+        password=PASSWORD,
+        host=HOST,
+        port=PORT,
+        dbname=DBNAME
+    )
+    st.write("Connection successful!")
+    
+    # Create a cursor to execute SQL queries
+    cursor = connection.cursor()
+    
+    # Example query
+    cursor.execute("SELECT NOW();")
+    result = cursor.fetchone()
+    print("Current Time:", result)
+
+    # Close the cursor and connection
+    cursor.close()
+    connection.close()
+    st.write("Connection closed.")
+
+except Exception as e:
+    st.write(f"Failed to connect: {e}")
+
 st.title('Test')
