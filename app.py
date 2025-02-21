9b558da38e49d393eba3afb0ee45746b34071343
brylapl <92885606+brylapl@users.noreply.github.com>
2025-02-21 12:19:30 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,16 @@
 import streamlit as st
-
 import psycopg2
 from dotenv import load_dotenv
 import os
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import StaleElementReferenceException
+import pandas as pd
 
 # Load environment variables from .env
 load_dotenv()
@@ -13,11 +21,21 @@ PASSWORD = st.secrets["postgresql"]["password"]
 HOST = st.secrets["postgresql"]["host"]
 PORT = st.secrets["postgresql"]["port"]
 DBNAME = st.secrets["postgresql"]["dbname"]
-st.write(HOST)
+
+def web_driver():
+    options = Options()
+    options.add_argument('--verbose')
+    options.add_argument('--no-sandbox')
+    options.add_argument("--headless=new")
+    options.add_argument('--disable-gpu')
+    options.add_argument("--window-size=1920x1080")
+    options.add_argument('user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.84 Safari/537.36')
+    driver = webdriver.Chrome(options=options)
+    return driver
 
 # Connect to the database
 try:
-    connection = psycopg2.connect(
+    conn = psycopg2.connect(
         user=USER,
         password=PASSWORD,
         host=HOST,
@@ -27,17 +45,57 @@ try:
     st.write("Connection successful!")
     
     # Create a cursor to execute SQL queries
-    cursor = connection.cursor()
+    c = conn.cursor()
     
     # Example query
-    cursor.execute("SELECT * FROM match")
-    result = cursor.fetchone()
+    c.execute("SELECT * FROM match")
+    result = c.fetchone()
     st.write('Data:', result[3])
     st.write('Home:', result[4])
 
+    driver = web_driver()
+
+    driver.get('https://www.flashscore.pl/')
+    all_match = driver.find_elements(By.XPATH,'//*[@title="Zobacz szczegóły meczu!"]')
+    data = data.split(' ')[0]
+    data = f'{data}/2025'
+    data = datetime.strptime(data, "%d/%m/%Y")
+    data = data.strftime("%Y-%m-%d")
+    ilosc = len(all_match)data = driver.find_element(By.XPATH,'//button[@id="calendarMenu"]').text
+    tabela = []
+
+    for mecz in all_match[0:6]:
+            liga = mecz.find_element(By.XPATH,'./preceding::div[contains(@class,"wclLeagueHeader")][1]/div[2]/div[1]/div[2]/span[1]').text
+            rozgrywki = mecz.find_element(By.XPATH,'./preceding::div[contains(@class,"wclLeagueHeader")][1]/div[2]/div[1]/div[2]/a').text
+            id = mecz.find_element(By.XPATH,'./..')
+            id = id.get_attribute('id')
+            id = id[4:]
+            ilosc -= 1
+            url = f'https://www.flashscore.pl/mecz/{id}/#/zestawienie-kursow/kursy-1x2/koniec-meczu'
+            time = mecz.find_element(By.XPATH,'./following-sibling::div[1]').text
+            home = mecz.find_element(By.XPATH,'./following-sibling::div[2]').text
+            away = mecz.find_element(By.XPATH,'./following-sibling::div[3]').text
+            stats = {
+                'Data': data,
+                'Time':time,
+                'Liga': liga,
+                'Rozgrywki':rozgrywki,
+                'Home': home,
+                'Away': away,
+                'Url':url,
+                }
+            tabela.append(stats)
+    df = pd.DataFrame(tabela)
+
+    records_home = df.to_records(index=False)
+    list_of_tuples_home = list(records_home)
+
+    c.executemany('INSERT INTO match (data, time, home, away, url) VALUES (?,?,?,?,?)',list_of_tuples_home)
+    conn.commit()
+
     # Close the cursor and connection
-    cursor.close()
-    connection.close()
+    c.close()
+    conn.close()
     st.write("Connection closed.")
 
 except Exception as e:
