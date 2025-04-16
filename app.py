6e1d508bf9f68e3050829a93f1b71f494c1ebdc3
brylapl <92885606+brylapl@users.noreply.github.com>
2025-04-16 12:23:34 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,39 +1,14 @@
 import streamlit as st
 import streamlit.components.v1 as components
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import StaleElementReferenceException
 
-
-def web_driver():
-    options = webdriver.ChromeOptions()
-    options.add_argument('--verbose')
-    options.add_argument('--no-sandbox')
-    options.add_argument('--headless')
-    options.add_argument('--disable-gpu')
-    options.add_argument('--disable-dev-shm-usage')
-    options.add_argument("--enable-javascript")
-    options.add_argument("--incognito")
-    options.add_argument("--nogpu")
-    options.add_argument("--window-size=1920x1080")
-    options.add_experimental_option("excludeSwitches", ["enable-automation"])
-    options.add_experimental_option('useAutomationExtension', False)
-    options.add_argument('--disable-blink-features=AutomationControlled')
-    options.add_argument('user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.84 Safari/537.36')
-    driver = webdriver.Chrome(options=options)
-    return driver
-
-
-driver = web_driver() 
-
-klik = st.button("Uruchom")
-if klik:
-  driver.get('https://m.flashscore.pl/') 
-  st.write('otwarto url') 
-  st.write(driver.title) 
-  driver.close()
+st.markdown(''' 
+<head>
+<style>
+h1 {
+text-align:center;
+color:red;
+} 
+</style>
+</head>
+''',unsafe_allow_html=True)
+st.markdown('<h1>Hello</h1>',unsafe_allow_html=True)
