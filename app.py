00e1b40140969fff9ed4617f01f4ad6ede47c1a0
brylapl <92885606+brylapl@users.noreply.github.com>
2025-04-16 11:52:36 +0200

    Update app.py

--- app.py
+++ app.py
@@ -10,4 +10,30 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import StaleElementReferenceException
 
 
-st.button("Uruchom")
+def web_driver():
+    options = webdriver.ChromeOptions()
+    options.add_argument('--verbose')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--headless')
+    options.add_argument('--disable-gpu')
+    options.add_argument('--disable-dev-shm-usage')
+    options.add_argument("--enable-javascript")
+    options.add_argument("--incognito")
+    options.add_argument("--nogpu")
+    options.add_argument("--window-size=1920x1080")
+    options.add_experimental_option("excludeSwitches", ["enable-automation"])
+    options.add_experimental_option('useAutomationExtension', False)
+    options.add_argument('--disable-blink-features=AutomationControlled')
+    options.add_argument('user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.84 Safari/537.36')
+    driver = webdriver.Chrome(options=options)
+    return driver
+
+
+driver = web_driver() 
+
+klik = st.button("Uruchom")
+if klik:
+  driver.get('https://m.flashscore.pl/') 
+  st.write('otwarto url') 
+  st.write(driver.title) 
+  driver.close()
