2bd2851ef0580ecfc22263b5d7a3afb8467d197e
brylapl <92885606+brylapl@users.noreply.github.com>
2025-04-16 11:48:51 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
+import streamlit as st
+import streamlit.components.v1 as components
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import StaleElementReferenceException
 
+
+st.button("Uruchom")
